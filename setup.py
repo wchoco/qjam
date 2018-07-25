25714ab0283a1bda05de061f88869d9343e9cbf2
riku.okajima <riku.okajima@gmail.com>
2018-07-25 17:07:42 +0900

    chore: update setup.py version
    0.0.1 -> 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qjam",
-    version="0.0.1",
+    version="0.1.0",
     description="UGE qsub command wrapper",
     author="riku.okajima",
     author_email="riku.okajima@gmail.com",
